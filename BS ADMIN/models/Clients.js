4f3c75955ff19272b9226267aff44c098a61e4c0
bader-js <sky.bader@gmail.com>
2020-01-08 17:06:15 +0100

    adding dashboard admin

---
+++ BS ADMIN/models/Clients.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+
+// Create Client Schema
+
+const ClientsSchema = new Schema({
+    user: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+
+    rôle: {
+        type: String
+    },
+    
+});
+
+
+module.exports = Clients = mongoose.model('bader', ClientsSchema)
